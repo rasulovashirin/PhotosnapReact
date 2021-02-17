390bd8d3e38a1b70c76d70c98ab337cce89a39d3
rasulovashirin <shirinrasulova1111@gmail.com>
2021-02-17 12:17:15 +0500

    add the all components into index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './Components/Generals.css'
+
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Main />
+    <Footer />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
